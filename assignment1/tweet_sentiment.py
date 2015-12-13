76cc4accb9a9280b55c8a39e3bc5c864afe08275
Rebecca Sweger <rebecca.sweger@gsa.gov>
2015-12-13 10:46:04 -0500

    assignment 1, problem 2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,24 @@
 import sys
+import json
 
-def hw():
-    print ('Hello, world!')
+def load_sentiments(sent_file):
+    with open(sent_file) as s:
+        rows = (line.split('\t') for line in s)
+        d = {row[0]:int(row[1].strip()) for row in rows}
+    return d
 
-def lines(fp):
-    print (str(len(fp.readlines())))
-
-def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+def main(sent_file, tweet_file):
+    sentiment_dict = load_sentiments(sent_file)
+    with open(tweet_file) as t:
+        for line in t:
+            try:
+                words = json.loads(line)['text'].split()
+            except:
+                #assume objects w/o 'text' attribute aren't tweets
+                continue
+            score = sum((
+                sentiment_dict[w] if w in sentiment_dict else 0 for w in words))
+            print (score)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1], sys.argv[2])
